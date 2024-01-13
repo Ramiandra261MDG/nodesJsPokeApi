5239c4443188cab21b2e5470da4a242d04f72fbe
Ramiandra261MDG <149253391+Ramiandra261MDG@users.noreply.github.com>
2024-01-14 00:17:15 +0300

    Update sequelize.js timezone delete

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -9,9 +9,6 @@ const config = require('../config/config')
 const sequelize = new Sequelize(config.DB_DBNAME, config.DB_USERNAME, config.DB_PASSWORD, {
   host: config.DB_HOST,
   dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Etc/GMT-2',
-  },
   logging: false
 })
 
